52589a91878d0ed632627a64550a07632604e317
regality <regality@gmail.com>
2011-03-25 11:03:23 -0700

    changed config

--- brainz/config.php
+++ brainz/config.php
@@ -7,10 +7,10 @@ $domain = 'zombiephp.com';
 
 $db_class = 'MySqlConnection';
 $db_file = 'sql/mysql_connection.php';
-$db_server = '/tmp/mysql/yuriko.sock';
-$db_user = 'marilyn';
-$db_pass = 'eNuikm8B';
-$database = 'yuriko';
+$db_server = '/tmp/mysql/annamarie.sock';
+$db_user = 'denese';
+$db_pass = 'yfSLiEad';
+$database = 'annamarie';
 
 $sess_file = 'session/php_session.php';
 $sess_class = 'PhpSession';
